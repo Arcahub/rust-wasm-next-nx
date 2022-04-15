4422c66600e150770bc5f1b878eead138509849c
Arcarox <arcarox@localhost.localdomain>
2022-04-15 18:56:35 +0200

    feat: add wasm to webpack config

--- apps/my-next-app/next.config.js
+++ apps/my-next-app/next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  webpack(config) {
+    // config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { asyncWebAssembly: true };
+
+    return config;
+  },
 };
 
 module.exports = withNx(nextConfig);
