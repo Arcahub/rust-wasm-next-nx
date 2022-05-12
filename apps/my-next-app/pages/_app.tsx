ff8f68f48c8a94c26a647636de7dcede2f44fa7f
Arcarox <arcarox@localhost.localdomain>
2022-05-13 01:21:09 +0200

    fix: format files

--- apps/my-next-app/pages/_app.tsx
+++ apps/my-next-app/pages/_app.tsx
@@ -4,22 +4,23 @@ import './styles.css';
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-export const RustComponent = dynamic({
-  loader: async () => {
-    // Import the wasm module
-    const module = await import('@rust-wasm-next/my-rust-lib');
+export const RustComponent = dynamic(
+  {
+    loader: async () => {
+      // Import the wasm module
+      const module = await import('@rust-wasm-next/my-rust-lib');
 
-    // Return a React component that calls the add_one method on the wasm module
-    return React.memo(
-      function RustComponent() {
+      // Return a React component that calls the add_one method on the wasm module
+      return React.memo(function RustComponent() {
         module.greet();
-        return <div>{}</div>
-      }
-    )
+        return <div>{}</div>;
+      });
+    },
   },
-}, {
-  ssr: false,
-})
+  {
+    ssr: false,
+  }
+);
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
